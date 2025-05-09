3759f6606c1750f4a8d392d373d6e5fb7fc73fdc
Gamson3 <gamson3@yahoo.com>
2025-05-09 09:25:59 +0200

    Use secure env vars (non-VITE_) in admin SDK for deployment

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -1,15 +1,15 @@
 // src/lib/firebase/admin.ts
 import admin from 'firebase-admin';
-import { VITE_FIREBASE_PRIVATE_KEY, VITE_FIREBASE_CLIENT_EMAIL, VITE_FIREBASE_PROJECT_ID } from '$env/static/private';
+import { FIREBASE_PRIVATE_KEY, FIREBASE_CLIENT_EMAIL, FIREBASE_PROJECT_ID } from '$env/static/private';
 import { getFirestore } from 'firebase-admin/firestore';
 
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: VITE_FIREBASE_PROJECT_ID,
-      clientEmail: VITE_FIREBASE_CLIENT_EMAIL,
-      privateKey: VITE_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+      projectId: FIREBASE_PROJECT_ID,
+      clientEmail: FIREBASE_CLIENT_EMAIL,
+      privateKey: FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     }),
   });
 }
